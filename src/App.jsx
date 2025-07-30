f7fb665d960616b548cb759f762f443f98791e0f
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-07-30 18:41:29 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Comp4 from './Comp4.jsx';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <h1>App komponentas</h1>
+        <Comp4 />
+      </div>
+    </>
+  );
+}
+  
+export default App
---
+++ src/Comp4.jsx
@@ -0,0 +1,5 @@
+function Comp4() {
+    return <p>Cia yra "Komp4" komponentas</p>
+}
+
+export default Comp4;
